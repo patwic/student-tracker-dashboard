0052d8a9c98dbc268495977dc3e4e2997698dd21
MeganMFisher <meganmichelle318@gmail.com>
2017-04-05 12:12:03 -0600

    added dropdown

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -1,3 +1,15 @@
 angular.module('app').controller('mainCtrl', function($scope){
   $scope.user= 'Jeremy Robertson'
+  $scope.isDropdown = false;
+
+  $scope.showDropdown = function() {
+      if (!$scope.isDropdown) {
+        document.getElementById('dropdown').classList.add('dropdown-transition')
+        $scope.isDropdown = true
+      }
+      else {
+        document.getElementById('dropdown').classList.remove('dropdown-transition')        
+        $scope.isDropdown = false
+      }
+  }
 })
