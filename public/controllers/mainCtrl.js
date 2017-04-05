6beb56c6e87fffa5d72a6b714b83c8f3a81ebfc6
MeganMFisher <meganmichelle318@gmail.com>
2017-04-05 12:29:55 -0600

    nav-bar cleanup

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -5,11 +5,10 @@ angular.module('app').controller('mainCtrl', function($scope){
   $scope.showDropdown = function() {
       if (!$scope.isDropdown) {
         document.getElementById('dropdown').classList.add('dropdown-transition')
-        $scope.isDropdown = true
       }
       else {
         document.getElementById('dropdown').classList.remove('dropdown-transition')        
-        $scope.isDropdown = false
       }
+      $scope.isDropdown = !$scope.isDropdown
   }
 })
