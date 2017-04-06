97f0ba63665ac405f308be9929f6e1271fcf831e
MeganMFisher <meganmichelle318@gmail.com>
2017-04-06 09:47:42 -0600

    login complete

--- public/app.js
+++ public/app.js
@@ -2,22 +2,34 @@ angular.module('app', ['ui.router'])
 
 .config(function($stateProvider, $urlRouterProvider){
 
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/root');
 
     $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: "./views/home.html"
-        })
-
-        .state('cohort', {
-            url: '/cohort',
-            templateUrl: "./views/cohort.html"
-        })
-
         .state('login', {
             url: '/login',
             templateUrl: "./views/login.html"
         })
+
+        .state('root', {
+            abstract: true,
+            views: {
+                '@': {
+                    template: '<ui-view />'
+                }
+            }
+        })
+
+        .state('root.home', {
+            parent: 'root',
+            url: '/',
+            templateUrl: "./views/home.html"
+        })
+
+        .state('root.cohort', {
+            parent: 'root',
+            url: '/cohort',
+            templateUrl: "./views/cohort.html"
+        })
+
 
 });
