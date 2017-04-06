4614f62537543589ff33d1e46cbb39d3a5f14b05
Patrick Holley <patrick_holley@outlook.com>
2017-04-06 10:23:53 -0600

    made daily function, updating totalQ if fetched data is bigger

--- server/questions.js
+++ server/questions.js
@@ -1,11 +1,23 @@
 const config = require('./config.js')
       request = require('request')
+      app = require('./server')
 
 module.exports = {
     getCurrentQ: (req, res) => {
         let day = new Date().toISOString().substring(0, 10)
         request.get(
             `${config.dev_mtn_api}historical/questions/?admin_token=${config.admin_token}&after=${day}`,
-            (err, qres, qbody) => res.status(200).send(qbody))
+            (err, qres, qbody) => {
+                if (err) console.log(err)
+                else {
+                    qbody = JSON.parse(qbody)
+                    let oldQ = app.getTotalQ()
+                    let newQ = []
+                    if (qbody.length > oldQ.length) {
+                        newQ = oldQ.concat(qbody.slice(oldQ.length))
+                        app.setTotalQ(newQ)
+                    }
+                }
+            })
     }
 }
--- server/server.js
+++ server/server.js
@@ -2,12 +2,31 @@ const express = require('express')
       bodyParser = require('body-parser')
       questions = require('./questions')
       config = require('./config.js')
-      app = express()
+      app = module.exports = express()
       port = 3000
+
+let totalQ = []
+    date = new Date().toISOString().substring(0, 10)
+
+setTimeout(function dailyTasks() {
+      let currentDate = new Date().toISOString().substring(0, 10)
+      if (date != currentDate) {
+            date = currentDate
+            totalQ = []
+      }
+      else console.log('Same day')
+      setTimeout(dailyTasks, 43200000)
+}, 43200000)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.getTotalQ = () => {return totalQ}
+app.setTotalQ = (newQ) => {
+      totalQ = newQ
+      console.log(totalQ)
+}
+
 app.get('/q/current', questions.getCurrentQ)
 
-app.listen(port, () => console.log(`Listening on port ${port} . . .`))
+app.listen(port, () => console.log(`Today is ${date}. Listening on port ${port} . . .`))
