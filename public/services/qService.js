b1a6f532a5d9fc61aef981dfd13a1fadf0a0de85
Patrick Holley <Patrick Holley>
2017-04-09 20:31:24 -0600

    still working on dividing days; successfully built day array between two dates

--- public/services/qService.js
+++ public/services/qService.js
@@ -1,6 +1,4 @@
 angular.module('app').service('qService', function($http, config) {
-    const apiUrl = 'http://q.devmountain.com/admin/'
-
     this.getQ = (beginDate, endDate, cohortId) => {
         let url = `${config.dev_mtn_api}historical/questions/?admin_token=${config.admin_token}&after=${beginDate}&before=${endDate}`
         if (cohortId) url += `&cohortId=${cohortId}`
@@ -15,7 +13,7 @@ angular.module('app').service('qService', function($http, config) {
         let endFD = this.dateFormatter(endDate)
         let dateArray = []
         for (let d = beginFD; d < endFD; d.setDate(d.getDate() + 1)) {
-            if (d.getDay() != 0 || d.getDay() != 6) dateArray.push(d)
+            if (d.getDay() != 0 && d.getDay() != 6) dateArray.push(new Date(d))
         }
         return dateArray
     }
