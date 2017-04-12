daa63c4872fb024e268c842e19df1604db3293ed
MeganMFisher <meganmichelle318@gmail.com>
2017-04-12 12:31:12 -0600

    box shadow on nav

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -53,6 +53,7 @@ angular.module('app').controller('mainCtrl', function ($scope, qService) {
     document.getElementById("cohort-sidenavStudent").style.width = "420px";
     document.getElementById("cohort-selectedCohort").style.backgroundColor = "#1a1a1a";
     document.getElementById("cohort-selectedCohort").style.color = '#25aae1';
+    document.getElementById("cohort-sidenav").style.boxShadow = "none";
   }
 
   $scope.closeCohortStudentNav = function () {
@@ -63,4 +64,59 @@ angular.module('app').controller('mainCtrl', function ($scope, qService) {
   $(function() {
     $('input[name="daterange"]').daterangepicker();
 });
+
+
+$('select').each(function () {
+
+    var $this = $(this),
+        numberOfOptions = $(this).children('option').length;
+
+    $this.addClass('s-hidden');
+
+    $this.wrap('<div class="select"></div>');
+
+    $this.after('<div class="styledSelect"></div>');
+
+    var $styledSelect = $this.next('div.styledSelect');
+
+    $styledSelect.text($this.children('option').eq(0).text());
+
+    var $list = $('<ul />', {
+        'class': 'options'
+    }).insertAfter($styledSelect);
+
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+
+    var $listItems = $list.children('li');
+
+    $styledSelect.click(function (e) {
+        e.stopPropagation();
+        $('div.styledSelect.active').each(function () {
+            $(this).removeClass('active').next('ul.options').hide();
+        });
+        $(this).toggleClass('active').next('ul.options').toggle();
+    });
+
+    $listItems.click(function (e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+    });
+
+    $(document).click(function () {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+});
+
+
+
+
 })
