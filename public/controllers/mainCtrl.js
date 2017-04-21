9578d8fde420cee95f9484e5c6d83753c27dc2ab
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-04-20 20:17:51 -0600

    fixed bug with home and cohort underline

--- public/controllers/mainCtrl.js
+++ public/controllers/mainCtrl.js
@@ -151,9 +151,9 @@ angular.module('app').controller('mainCtrl', function ($scope, attendanceService
     $scope.isDropdown = !$scope.isDropdown
   }
 
-  if ($location.path() === '/') {
-    $scope.activateLink = true;
-  } else $scope.activateLink = false;
+  if ($location.path() === '/cohort') {
+    $scope.activateLink = false;
+  } else $scope.activateLink = true;
 
   $scope.changeLink = function (status) {
     $scope.activateLink = status;
