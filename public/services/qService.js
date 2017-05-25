3cef5d568525569b195bd4618f5263c173d0d47d
MeganMFisher <meganmichelle318@gmail.com>
2017-05-25 14:05:07 -0600

    slideUps doneZo!

--- public/services/qService.js
+++ public/services/qService.js
@@ -309,7 +309,7 @@ angular.module('app').service('qService', function ($http, config) {
 
     this.sortHelpedTotals = (students) => {
         students.map((e) => {
-           return e.sum = Math.floor(e.sum / 60)
+           return e.sum = Math.round(e.sum / 60000)
         })
         return students.sort((a,b) => b.sum - a.sum)
     }
