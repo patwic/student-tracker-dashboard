fa68c7f07ab9bc0991c42cf555bb28c71e4a6e8c
meganmfisher <meganmichellefisher@gmail.com>
2017-10-19 12:30:02 -0600

    surveys

---
+++ public/directives/cohortSurveyBarChart.js
@@ -0,0 +1,199 @@
+angular.module('app')
+.directive('barChart', function () {
+  return {
+    templateUrl: '<div id="barChart"></div>',
+    scope: {},
+    controller: function ($scope, surveyData) {
+      $scope.survey = 'MSAT'
+      $scope.cohort = 'WPR23'
+      let survey = $scope.survey
+      let sd = surveyData.data
+      let filteredData = sd.filter(e => e.cohort === $scope.cohort);
+      let averages = (dataArr) => {
+        let arr = []
+        let obj = {}
+        let max = 0;
+        let min = 13;
+        for (let i = 0; i < dataArr.length; i++) {
+          let u = dataArr[i].unit
+          if (u > max) max = u
+          if (u < min) min = u
+          let d = dataArr[i]
+          if (!obj[u]) obj[u] = {}
+          obj[u].CSAT = obj[u].CSAT ? obj[u].CSAT + d.CSAT : d.CSAT
+          obj[u].FSAT = obj[u].FSAT ? obj[u].FSAT + d.FSAT : d.FSAT
+          obj[u].MSAT = obj[u].MSAT ? obj[u].MSAT + d.MSAT : d.MSAT
+          obj[u].OSAT = obj[u].OSAT ? obj[u].OSAT + d.OSAT : d.OSAT
+          obj[u].CSATcount = obj[u].CSATcount ? obj[u].CSATcount += 1 : 1
+          obj[u].FSATcount = obj[u].FSATcount ? obj[u].FSATcount += 1 : 1
+          obj[u].MSATcount = obj[u].MSATcount ? obj[u].MSATcount += 1 : 1
+          obj[u].OSATcount = obj[u].OSATcount ? obj[u].OSATcount += 1 : 1
+        }
+        for (let i = min; i <= max; i++) {
+          obj[i].CSAT = (obj[i].CSAT / obj[i].CSATcount).toFixed(2)
+          obj[i].FSAT = (obj[i].FSAT / obj[i].FSATcount).toFixed(2)
+          obj[i].MSAT = (obj[i].MSAT / obj[i].MSATcount).toFixed(2)
+          obj[i].OSAT = (obj[i].OSAT / obj[i].OSATcount).toFixed(2)
+          obj[i].unit = i
+          arr.push(obj[i])
+        }
+        return arr
+      }
+      let data = averages(filteredData)
+      var margin = {
+        top: 100,
+        right: 30,
+        bottom: 40,
+        left: 50
+      }
+      let num = 9;
+    //   var height = document.getElementById('barChart').offsetHeight - 100 - margin.top - margin.bottom;
+    //   var width = document.getElementById('barChart').offsetWidth - margin.right - margin.left;
+      var height = 400;
+      var width = 600;
+      var x = d3.scaleBand()
+        .domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+        .range([0, width])
+        .padding(.2)
+      var y = d3.scaleLinear()
+        .domain([0, 10])
+        .range([height, 0]);
+      var xAxis = d3.axisBottom(x);
+      var yAxis = d3.axisLeft(y)
+      var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-15, 0])
+        .html(function (d) {
+          let count = survey + 'count'
+          return "Average Rating: <span style='color:#21AAE1; line-height: 1.5;'> " + d[survey] + "</span>" + "<br>" + "Responded: <span style='color:#21AAE1; line-height: 1.5;'> " + d[count] + "</span>"
+        })
+        .style('font-size', '11px')
+      var svg = d3.select("#barChart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      svg.call(tip);
+      svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .style("text-anchor", "end")
+      svg.append('g')
+        .attr('class', 'x axis')
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+      svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) {
+          return x(d.unit);
+        })
+        .attr("width", x.bandwidth())
+        .attr("y", height)
+        .attr("height", function (d) {
+          return 0;
+        })
+        .attr('fill', function (d) {
+          if (d[survey] >= num) return '#21AAE1'
+          else return '#252525';
+        })
+        .on('mouseover', function (d) {
+          tip.show(d)
+          if (d[survey] >= num) {
+            d3.select(this)
+              .attr("fill", "#297FAA");
+          } else {
+            d3.select(this)
+              .attr("fill", "#000");
+          }
+        })
+        .on('mouseout', function (d) {
+          tip.hide(d)
+          if (d[survey] >= num) {
+            d3.select(this)
+              .attr("fill", '#21AAE1');
+          } else {
+            d3.select(this)
+              .attr("fill", "#252525");
+          }
+        })
+        .transition()
+        .duration(1000)
+        .attr("width", x.bandwidth())
+        .attr("y", function (d) {
+          return y(d[survey]);
+        })
+        .attr("height", function (d) {
+          return height - y(d[survey]);
+        })
+        .attr('fill', function (d) {
+          if (d[survey] >= num) return '#21AAE1'
+          else return '#252525';
+        })
+      $scope.changeBar = () => {
+        let num = 9;
+        let newSurvey = $scope.survey
+        let newFilteredData = sd.filter(e => e.cohort === $scope.cohort);
+        let newData = averages(newFilteredData)
+        var yD = d3.scaleLinear().domain([0, 10]).range([height - 20, 0]);
+        var tip = d3.tip()
+          .attr('class', 'd3-tip')
+          .offset([-15, 0])
+          .html(function (d) {
+            let count = newSurvey + 'count'
+            return "Average Rating: <span style='color:#21AAE1; line-height: 1.5;'> " + d[newSurvey] + "</span>" + "<br>" + "Responded: <span style='color:#21AAE1; line-height: 1.5;'> " + d[count] + "</span>"
+          })
+          .style('font-size', '11px')
+        svg.call(tip);
+        d3.selectAll('.bar')
+          .attr("y", height)
+          .attr("height", function (d) {
+            return 0;
+          })
+        d3.select('#barChart')
+          .selectAll(".bar")
+          .data(newData)
+          .attr("x", function (d) {
+            return x(d.unit);
+          })
+          .on('mouseover', function (d) {
+            tip.show(d)
+            if (d[newSurvey] >= num) {
+              d3.select(this)
+                .attr("fill", "#297FAA");
+            } else {
+              d3.select(this)
+                .attr("fill", "#000");
+            }
+          })
+          .on('mouseout', function (d) {
+            tip.hide(d)
+            if (d[newSurvey] >= num) {
+              d3.select(this)
+                .attr("fill", '#21AAE1');
+            } else {
+              d3.select(this)
+                .attr("fill", "#252525");
+            }
+          })
+          .transition()
+          .duration(1000)
+          .attr("width", x.bandwidth())
+          .attr("y", function (d) {
+            return yD(d[newSurvey]);
+          })
+          .attr("height", function (d) {
+            return height - yD(d[newSurvey]);
+          })
+          .attr('fill', function (d) {
+            if (d[newSurvey] >= num) return '#21AAE1'
+            else return '#252525';
+          })
+      }
+    }
+  }
+})
---
+++ public/directives/weeklySurveysScatterChart.js
@@ -0,0 +1,86 @@
+angular.module('app')
+.directive('surveyScatter', function () {
+  return {
+    template: '<div id="surveyScatter"></div>',
+    controller: function ($scope, surveyService) {
+      
+      $scope.survey = 'MSAT'
+      $scope.cohort = 'WPR23'
+      let survey = $scope.survey
+      let sd = surveyService.data
+      let filteredData = sd.filter(e => e.cohort === $scope.cohort);
+      let averages = (dataArr) => {
+        let arr = []
+        let obj = {}
+        let max = 0;
+        let min = 13;
+        for (let i = 0; i < dataArr.length; i++) {
+          let u = dataArr[i].unit
+          if (u > max) max = u
+          if (u < min) min = u
+          let d = dataArr[i]
+          if (!obj[u]) obj[u] = {}
+          obj[u].CSAT = obj[u].CSAT ? obj[u].CSAT + d.CSAT : d.CSAT
+          obj[u].FSAT = obj[u].FSAT ? obj[u].FSAT + d.FSAT : d.FSAT
+          obj[u].MSAT = obj[u].MSAT ? obj[u].MSAT + d.MSAT : d.MSAT
+          obj[u].OSAT = obj[u].OSAT ? obj[u].OSAT + d.OSAT : d.OSAT
+          obj[u].CSATcount = obj[u].CSATcount ? obj[u].CSATcount += 1 : 1
+          obj[u].FSATcount = obj[u].FSATcount ? obj[u].FSATcount += 1 : 1
+          obj[u].MSATcount = obj[u].MSATcount ? obj[u].MSATcount += 1 : 1
+          obj[u].OSATcount = obj[u].OSATcount ? obj[u].OSATcount += 1 : 1
+        }
+        for (let i = min; i <= max; i++) {
+          obj[i].CSAT = (obj[i].CSAT / obj[i].CSATcount).toFixed(2)
+          obj[i].FSAT = (obj[i].FSAT / obj[i].FSATcount).toFixed(2)
+          obj[i].MSAT = (obj[i].MSAT / obj[i].MSATcount).toFixed(2)
+          obj[i].OSAT = (obj[i].OSAT / obj[i].OSATcount).toFixed(2)
+          obj[i].unit = i
+          arr.push(obj[i])
+        }
+        return arr
+      }
+      let data = averages(filteredData)
+      
+      var margin = {
+        top: 50,
+        right: 40,
+        bottom: 20,
+        left: 80
+      }
+      // var width = document.getElementById('surveyScatter').offsetWidth - margin.right - margin.left;
+      // var height = document.getElementById('surveyScatter').offsetHeight - margin.top - margin.bottom - 80;
+      var height = 400;
+      var width = 600;
+      var x = d3.scalePoint()
+      .range([0, width]);
+      var y = d3.scalePoint()
+      .range([height, 0]);
+      var svg = d3.select("#surveyScatter").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+      x.domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+       
+      y.domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+       
+      svg.selectAll("dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("r", 4)
+        .attr("cx", function (d) {
+          return x(+d.unit);
+        })
+        .attr("cy", function (d) {
+          console.log(+d[survey])
+          return y(+d[survey]);
+        })
+      svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+      svg.append("g")
+        .call(d3.axisLeft(y));
+    }
+  }
+})
