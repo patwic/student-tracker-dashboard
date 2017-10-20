74c8d0ef97b60df0a0eeb2677691da72248177dd
meganmfisher <meganmichellefisher@gmail.com>
2017-10-20 16:14:12 -0600

    endpoint in server

--- public/controllers/surveysCtrl.js
+++ public/controllers/surveysCtrl.js
@@ -7,18 +7,21 @@ $scope.cohortStatus;
     getAllSurveyData = (status, column) => {
         $scope.surveyName = column || "OSAT"
         $scope.cohortStatus = status || 'all'
-
-        surveyService.getWeeklySurveyData().then(res => {
-            $scope.sd = res.data
-            console.log($scope.sd)
-        })
-
+        $scope.sd = surveyService.data
         $scope.cohortName = "WPR23" //While testing will need to use cohortStatus once endpoint is up and running.
         console.log($scope.surveyName)
     //     $scope.sd = surveyGraphData.filter(e => e.cohort === $scope.cohortName);
       }
 
     getAllSurveyData()
+
+    getAll = () => {
+        surveyService.getWeeklySurveyData().then(res => {
+            $scope.sd = res.data
+            console.log($scope.sd)
+        })
+    }
+    getAll()
 
     $scope.changeSurveyName = () => {
         getAllSurveyData($scope.cohortStatus, event.target.value)
--- server/survey.js
+++ server/survey.js
@@ -1,6 +1,13 @@
+const config = require('./config.js'),
+request = require('request-promise'),
+app = require('./server')
+
 module.exports = {
     getWeekly: (req, res) => {
-        console.log('hello')
+        request.get('https://surveys.devmountain.com/api/tableau/data', (err, resp, body) => {
+            console.log(resp)
+              res.send(resp)
+        })
     }
 }
 
