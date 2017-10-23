94e2b93245dfa0156c96cd52c69b3f51e5627fa9
meganmfisher <meganmichellefisher@gmail.com>
2017-10-23 07:41:46 -0600

    filtering through sat type working

--- public/controllers/surveysCtrl.js
+++ public/controllers/surveysCtrl.js
@@ -1,39 +1,21 @@
 angular.module('app').controller('surveysCtrl', function($scope, surveyService) {
 
 $scope.surveyName;
-$scope.cohortName;
 $scope.selectedProgram;
-
-
-// $scope.sd = surveyService.getWeeklySurveyData().then(res => {
-
-//     // console.log(res.data)
-//     let name = "ux" 
-
-//     let stuff = res.data.filter(e => {
-//        return  e.program === name   
-//     })
-
-//     $scope.sd = stuff
-//     console.log($scope.sd)
-// })
 
     getAllSurveyData = (program, column) => {
         $scope.surveyName = column || "OSAT"
         $scope.selectedProgram = program || 'ux'
 
         $scope.sd = surveyService.getWeeklySurveyData().then(res => {
-            
-                // console.log(res.data)
-                // let name =  program || "ux" 
-            
-                let stuff = res.data.filter(e => {
-                   return  e.program === $scope.selectedProgram   
-                })
-            
-                $scope.sd = stuff
-                console.log($scope.sd)
+
+            let stuff = res.data.filter(e => {
+                return  e.program === $scope.selectedProgram   
             })
+            
+            $scope.sd = stuff
+            console.log($scope.sd)
+        })
       }
 
     getAllSurveyData()
