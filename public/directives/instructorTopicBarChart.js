4c6e4bcd1909a147f94c2a989bb9bb55856acc76
meganmfisher <meganmichellefisher@gmail.com>
2017-11-01 15:16:43 -0600

    instructor bar chart showing with dummy data

--- public/directives/instructorTopicBarChart.js
+++ public/directives/instructorTopicBarChart.js
@@ -11,14 +11,27 @@ angular.module('app')
 
         let filteredData = [{
           "OSAT": 5,
-          "subtopic": 'w-1.3',
+          "subtopic": '1.1',
         },{
           "OSAT": 3,
-          "subtopic": 'w-1.1',
+          "subtopic": '1.2',
         },{
           "OSAT": 4,
-          "subtopic": 'w-1.2',
+          "subtopic": '1.3',
+        },{
+          "OSAT": 5,
+          "subtopic": '1.4',
+        },{
+          "OSAT": 3,
+          "subtopic": '1.5',
+        },{
+          "OSAT": 5,
+          "subtopic": '1.6',
         }]
+
+        xAxisData = filteredData.map(e => e.subtopic)
+
+        console.log(xAxis)
           
         let data = filteredData
 
@@ -29,11 +42,12 @@ angular.module('app')
           left: 50
         }
 
-        var width = document.getElementById('surveyInstructorBarChart').offsetWidth - margin.right - margin.left;
-        var height = document.getElementById('surveyInstructorBarChart').offsetHeight - margin.top - margin.bottom - 80;
+      let num = 4;
+      var width = document.getElementById('surveyInstructorBarChart').offsetWidth - margin.right - margin.left;
+      var height = document.getElementById('surveyInstructorBarChart').offsetHeight - margin.top - margin.bottom - 80;
 
-        var x = d3.scaleBand()
-        .domain([1, 2, 3, 4, 5])
+      var x = d3.scaleBand()
+        .domain(xAxisData)
         .range([0, width])
         .padding(.2)
       var y = d3.scaleLinear()
@@ -43,10 +57,10 @@ angular.module('app')
       var yAxis = d3.axisLeft(y);
 
       var svg = d3.select("#surveyInstructorBarChart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
       svg.append("g")
         .attr("class", "y axis")
@@ -64,17 +78,62 @@ angular.module('app')
         .enter().append("rect")
         .attr("class", "bar")
         .attr("x", function (d) {
-        return x(d.unit);
+        return x(d.subtopic);
       })
         .attr("width", x.bandwidth())
         .attr("y", height)
         .attr("height", function (d) {
         return 0;
       })
-     
-    changeBar = (surveyData) => {
+        .attr('fill', function (d) {
+          if (d.OSAT >= num) return '#21AAE1'
+          else return '#3d3d3d';
+        })
+        .transition()
+        .duration(1000)
+        .attr("width", x.bandwidth())
+        .attr("y", function (d) {
+          return y(d.OSAT);
+        })
+        .attr("height", function (d) {
+          return height - y(d.OSAT);
+        })
+        .attr('fill', function (d) {
+          if (d.OSAT >= num) return '#21AAE1'
+          else return '#3d3d3d';
+        })
 
+    changeBar = (surveyData) => {
         console.log(surveyData)
+        let num = 4;
+        let newData = filteredData;
+        
+        var yD = d3.scaleLinear().domain([0, 5]).range([height - 20, 0]);
+
+        d3.selectAll('.bar')
+          .attr("y", height)
+          .attr("height", function (d) {
+            return 0;
+          })
+        d3.select('#surveyInstructorBarChart')
+          .selectAll(".bar")
+          .data(newData)
+          .attr("x", function (d) {
+            return x(d.subtopic);
+          })
+          .transition()
+          .duration(1000)
+          .attr("width", x.bandwidth())
+          .attr("y", function (d) {
+            return yD(d.OSAT);
+          })
+          .attr("height", function (d) {
+            return height - yD(d.OSAT);
+          })
+          .attr('fill', function (d) {
+            if (d.OSAT >= num) return '#21AAE1'
+            else return '#3d3d3d';
+          })
 
       }
 
